fe44223a7728023fe6559ab28cdfdaf31c47f9a6
Richard <minecraftcom09@gmail.com>
2019-04-12 17:52:16 +0800

    Added declaration of variables

--- Vampirism Idle/Assets/Scripts/GameManager.cs
+++ Vampirism Idle/Assets/Scripts/GameManager.cs
@@ -4,10 +4,21 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    /*Declaration of Variables*/
+
+    //Resources
+    public double res_blood;
+    public double res_wood;
+    public double res_stone;
+    public double res_humanPop;
+    
+    
+
+
     // Start is called before the first frame update
     void Start()
     {
-        double d = 2;
+        
     }
 
     // Update is called once per frame
