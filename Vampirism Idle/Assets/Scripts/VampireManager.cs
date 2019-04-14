92122a9e3e2dc10d07a38fb27d04ed1e7ba6cc03
Richard <minecraftcom09@gmail.com>
2019-04-14 23:17:58 +0800

    Added more initialization

--- Vampirism Idle/Assets/Scripts/VampireManager.cs
+++ Vampirism Idle/Assets/Scripts/VampireManager.cs
@@ -6,6 +6,19 @@ public class VampireManager : MonoBehaviour
 {
     /* This contains the stats of every vampire */
 
+        /*Vampire List
+         * Fledgeling
+         * Vampling
+         * Lesser Vampire
+         * Greater Vampire
+         * Coven Lord
+         * Necromancer
+         * Highborne
+         * Royal Guard
+         * Blood King
+         * The Father
+         */
+
     public string[] vampires_name;
     public double[] vampires_bloodEfficiency;
     public double[] vampires_maximumBloodGather;
@@ -28,6 +41,16 @@ public class VampireManager : MonoBehaviour
         vampires_bloodEfficiency[0] = 75 / 100;
         vampires_baseCost_sharpenFangs[0] = 5000;
         vampires_baseCost_trainAgility[0] = 7500;
+
+        vampires_Cost_sharpenFangs[0] = 0;
+        vampires_Cost_trainAgility[0] = 0;
+        vampires_level_sharpenFangs[0] = 0;
+        vampires_level_trainAgility[0] = 0;
+
+        vampires_amount_Total[0] = 0;
+        vampires_amount_Used_Total[0] = vampires_amount_Used_Feed[0] + vampires_amount_Used_Infect[0];
+        vampires_amount_Used_Feed[0] = 0;
+        vampires_amount_Used_Infect[0] = 0;
 
         
     }
