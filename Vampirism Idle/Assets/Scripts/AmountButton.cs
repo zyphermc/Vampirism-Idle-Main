afedccd087de427063dbb8f4a75eb2c4f18e9761
Richard <minecraftcom09@gmail.com>
2019-04-20 06:59:44 +0800

    Added some UI Buttons
    - added Ascend button (UI only, no function yet)
    - added Amount Button

---
+++ Vampirism Idle/Assets/Scripts/AmountButton.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class AmountButton : MonoBehaviour
+{
+    public TextMeshProUGUI textBox_AmountButton;
+    public int[] amountNumber;
+    public int amountIndex;
+
+    public void ChangeAmount()
+    {
+        if(amountIndex != 6)
+        {
+            amountIndex++;
+        }
+        else
+        {
+            amountIndex = 0;
+        }
+
+        if(amountIndex < 6)
+        {
+            textBox_AmountButton.text = (amountNumber[amountIndex] + "x");
+        }
+        else
+        {
+            textBox_AmountButton.text = ("MAX");
+        }
+
+        Debug.Log("Amount Index: " + amountIndex);
+    }
+}
