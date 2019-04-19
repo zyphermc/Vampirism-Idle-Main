101989426bc182ba95a5a0c662c84e6e99cf284a
Richard <minecraftcom09@gmail.com>
2019-04-19 10:24:10 +0800

    Added some Bite Button functionality
    - added cooldown timer to bite button (currently 10s)

--- Vampirism Idle/Assets/Scripts/BiteButton.cs
+++ Vampirism Idle/Assets/Scripts/BiteButton.cs
@@ -1,9 +1,17 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
-public class BiteButton : MonoBehaviour
+public class BiteButton : MonoBehaviour //Local
 {
     public GameManager GameManager;
     public VampireManager VampireManager;
+    public Button button_biteButton;
+    public TextMeshProUGUI textBox_biteButton;
+
+    private bool cooldown = false;
+    private int ticks = 11;
 
     public void biteHuman()
     {
@@ -11,6 +19,42 @@ public class BiteButton : MonoBehaviour
         {
             VampireManager.vampires_amount_Total[0] += 1;
             GameManager.res_HumanPop -= 1;
+            StartCoroutine("BiteCooldown");
+            cooldown = true;
         }
+    }
+
+    IEnumerator BiteCooldown()
+    {
+        ticks = 11;
+        button_biteButton.interactable = false;
+
+        while (true)
+        {
+            ticks--;
+
+            if(ticks <= 0)
+            {
+                cooldown = false;
+                button_biteButton.interactable = true;
+                StopCoroutine("BiteCooldown");
+            }
+
+            yield return new WaitForSeconds(1f);
+        }
+        
+    }
+
+    private void Update()
+    {
+        if(cooldown == false)
+        {
+            textBox_biteButton.text = "Bite";
+        }
+        else
+        {
+            textBox_biteButton.text = ticks + "s";
+        }
+        
     }
 }
