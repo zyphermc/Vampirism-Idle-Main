98f620ad5156946b7010ff6ec7e715c2ca922ff9
Richard <minecraftcom09@gmail.com>
2019-04-30 22:49:21 +0800

    UI and News Box Update
    - Changed the graphics for Vamp Buttons
    - Changed the graphics for News Box
    - Added News Box functionality
    - Added filler news
    - Added scrolling text to news box

---
+++ Vampirism Idle/Assets/Scripts/News Box Code/NewsItem.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NewsItem : MonoBehaviour
+{
+    private float itemWidth;
+    private float pixelsPerSecond;
+    private RectTransform rt;
+
+    public float getXPos() //return anchored x position
+    {
+        return rt.anchoredPosition.x;
+    }
+
+    public float getWidth()
+    {
+        return rt.rect.width;
+    }
+
+    public void Initialize(float itemWidth, float pixelsPerSecond, string message)
+    {
+        this.itemWidth = itemWidth;
+        this.pixelsPerSecond = pixelsPerSecond;
+        rt = GetComponent<RectTransform>();
+        GetComponent<TextMeshProUGUI>().text = message;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rt.position += Vector3.left * pixelsPerSecond * Time.deltaTime;
+        if(getXPos() <= 0 - itemWidth - getWidth())
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Vampirism Idle/Assets/Scripts/News Box Code/NewsTicker.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewsTicker : MonoBehaviour
+{
+    public NewsItem newsItem_prefab; //The news text prefab
+    [Range(0,10)]public float itemDuration; //How long to scroll the news
+    public string[] fillerItems; //News that will loop if there is no important news to show
+
+    private float width;
+    private float pixelsPerSecond; //to calculate the speed in which the whole news scrolls
+    private NewsItem currentItem;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        width = GetComponent<RectTransform>().rect.width;
+        pixelsPerSecond = width / itemDuration;
+        AddNewsItem(fillerItems[0]);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!currentItem)
+        {
+            AddNewsItem(fillerItems[Random.Range(0, 2)]);
+        }
+    }
+
+    private void AddNewsItem(string message)
+    {
+        currentItem = Instantiate(newsItem_prefab, transform);
+        currentItem.Initialize(width, pixelsPerSecond, message);
+        currentItem.transform.SetAsFirstSibling();
+    }
+}
