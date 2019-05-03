f10f539b90410c07cc54aacc27901f20fd682528
Richard <minecraftcom09@gmail.com>
2019-05-04 00:20:27 +0800

    Shorten Number Update
    - Added Post-fix method for shortening
    - Added Scientific Notation method for shortening

--- Vampirism Idle/Assets/Scripts/ShortenNumber.cs
+++ Vampirism Idle/Assets/Scripts/ShortenNumber.cs
@@ -1,26 +1,61 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace NumberShortening
 {
     public class ShortenNumber
     {
-        public string shortenNumber(double value)
+        public string[] numberPostFix =
         {
-            int zeroAmount = (int)Math.Log10(value);
-            
-            if(zeroAmount > 2) //if more than hundredths
+            " Thousand",
+            " Million",
+            " Trillion"
+        };
+
+        public int postFixIndex; //index for post fix
+        public bool decreased = false; //checks if index is already decreased
+
+        public int shortenMethod = 0; //0 = post fix, 1 = scientific
+
+        public string shortenNumber(double value, int method)
+        {
+            if(method == 0) //if post fix method
             {
-                return value.ToString("0.00e0"); //scientific notation
+                /* Post Fix Notation */
+                int zeroAmount = (int)Math.Log10(value); //gets the amount of zeroes in value
+
+                if (zeroAmount > 2) //if more than hundredths
+                {
+                    if (zeroAmount % 3 == 0) //if multiple of 3
+                    {
+                        postFixIndex = (int)(zeroAmount / 3f); //get the index by dividing the amount of zeroes by 3
+                        decreased = false; //refresh boolean
+                    }
+                    else if (zeroAmount < (postFixIndex * 3) && decreased == false) //if value went below post fix (e.g. thousands, millions, etc.)
+                    {
+                        postFixIndex -= 1; //decrease the index by 1
+                        decreased = true; //made true to prevent further decrease
+                    }
+
+                    return (value / Mathf.Pow(10, postFixIndex * 3)).ToString("F2") + numberPostFix[postFixIndex - 1]; //return value in two decimal places with a post fix
+                }
+                else
+                {
+                    return value.ToString(); //return value un-edited
+                }
             }
-            else
+            else if(method == 1) //if scientific method
+            {
+                /* Scientific Notation */
+                return value.ToString("0.000e0"); //scientific notation
+            }
+            else //if neither, return it unedited
             {
                 return value.ToString();
             }
-            
+
+
+
         }
     }
-
 }
