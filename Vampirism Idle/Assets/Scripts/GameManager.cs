a1a515c79f2fbe19f1b1cfaa7ff9d5560a423f3f
Richard <minecraftcom09@gmail.com>
2019-05-07 23:07:49 +0800

    Game Update
    -Added functionality to window buttons

--- Vampirism Idle/Assets/Scripts/GameManager.cs
+++ Vampirism Idle/Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour //Persistent (contains game stats)
 {
@@ -14,6 +15,15 @@ public class GameManager : MonoBehaviour //Persistent (contains game stats)
 
     //Class Objects
     private ShortenNumber sn = new ShortenNumber();
+
+    //Panel Number Variable (0 = Lair, 1 = Buildings, and so on and so forth)
+    public int panelNum;
+
+    //Content GameObjects (for mass enabling and disabling)
+    public Button[] panel_Button;
+    public GameObject[] panel_Content;
+
+    public GameObject resourcePanel; //move accordingly to fit panel layout
 
     //Resources
     public double res_Blood;
@@ -34,8 +44,27 @@ public class GameManager : MonoBehaviour //Persistent (contains game stats)
     // Update is called once per frame
     private void Update()
     {
+        //Update Resource UI
         textBox_Blood.text = sn.shortenNumber(res_Blood, sn.shortenMethod, 2);
         textBox_HumanPop.text = sn.shortenNumber(res_HumanPop, sn.shortenMethod, 3);
+
+        //Update Panel Buttons
+        for (int a = 0; a < panel_Button.Length; a++)
+        {
+            if (panelNum == a)
+            {
+                panel_Button[a].enabled = false;
+                panel_Button[a].interactable = false;
+                panel_Content[a].SetActive(true);
+            }
+            else
+            {
+                panel_Button[a].enabled = true;
+                panel_Button[a].interactable = true;
+                panel_Content[a].SetActive(false);
+            }
+        }
+        
     }
 
     private IEnumerator UpdateResources()
@@ -50,5 +79,31 @@ public class GameManager : MonoBehaviour //Persistent (contains game stats)
     public void addHumans()
     {
         res_HumanPop += 1;
+    }
+
+    /* Panel Button Methods */
+    public void SwitchLair()
+    {
+        panelNum = 0;
+    }
+    public void SwitchBuildings()
+    {
+        panelNum = 1;
+    }
+    public void SwitchLabor()
+    {
+        panelNum = 2;
+    }
+    public void SwitchUpgrades()
+    {
+        panelNum = 3;
+    }
+    public void SwitchBattle()
+    {
+        panelNum = 4;
+    }
+    public void SwitchOptions()
+    {
+        panelNum = 5;
     }
 }
