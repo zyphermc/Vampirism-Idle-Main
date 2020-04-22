0382bb94851c93882e00bd34aa8e774879f2f7c8
Richard <minecraftcom09@gmail.com>
2020-04-23 02:29:55 +0800

    Save System Data Finished
    -Data to save are now complete.
    - Save System may need a rework.
    
    -Last commit in Unity 2019.2
    -If not upgraded to Unity 2019.3 yet, go upgrade.

--- Vampirism Idle/Assets/Scripts/Persistent/Save System Scripts/SaveSystem.cs
+++ Vampirism Idle/Assets/Scripts/Persistent/Save System Scripts/SaveSystem.cs
@@ -1,4 +1,6 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
 using System.Runtime.Serialization.Formatters.Binary;
 using UnityEngine;
 
@@ -14,38 +16,71 @@ public class SaveSystem : MonoBehaviour
 
     //------------------------------------
 
+    [DllImport("__Internal")]
+    private static extern void SyncFiles();
+
+    [DllImport("__Internal")]
+    private static extern void WindowAlert(string message);
+
     public void SaveData()
     {
         BinaryFormatter formatter = new BinaryFormatter(); //formats data into binary
         string path = Application.persistentDataPath + "/playerData.zyp"; //path where binary file is to be located/created
 
-        using (FileStream stream = new FileStream(path, FileMode.Create)) //automatically closes the filestream after code block is run
+        try
         {
+            FileStream stream = new FileStream(path, FileMode.Create); //automatically closes the filestream after code block is run
+
             PlayerData data = new PlayerData(gameManager, vampireManager, efficiencyUpgrades, housingManager, laborManager); //create an instance of PlayerData to access its content (so that I don't have to drag it as a component reference)
 
             formatter.Serialize(stream, data);
+            stream.Close();
+
+            if (Application.platform == RuntimePlatform.WebGLPlayer)
+            {
+                SyncFiles();
+            }
+        }
+        catch (Exception e)
+        {
+            PlatformSafeMessage("Failed to Save: " + e.Message);
         }
     }
 
     public PlayerData LoadData()
     {
+        PlayerData data = null;
         string path = Application.persistentDataPath + "/playerData.zyp";
 
-        if (File.Exists(path))
+        try
         {
-            BinaryFormatter formatter = new BinaryFormatter();
-
-            using (FileStream stream = new FileStream(path, FileMode.Open))
+            if (File.Exists(path))
             {
-                PlayerData data = formatter.Deserialize(stream) as PlayerData;
+                BinaryFormatter formatter = new BinaryFormatter();
 
-                return data;
+                FileStream stream = new FileStream(path, FileMode.Open);
+
+                data = formatter.Deserialize(stream) as PlayerData;
+                stream.Close();
             }
+            return data;
+        }
+        catch (Exception e)
+        {
+            PlatformSafeMessage("Failed to Load: " + e.Message);
+            return null;
+        }
+    }
+
+    private static void PlatformSafeMessage(string message)
+    {
+        if (Application.platform == RuntimePlatform.WebGLPlayer)
+        {
+            WindowAlert(message);
         }
         else
         {
-            Debug.LogError("Save file not found in path: " + path);
-            return null;
+            Debug.Log(message);
         }
     }
 }
